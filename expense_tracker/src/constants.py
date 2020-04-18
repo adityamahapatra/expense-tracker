c6abd8eb94d5d968f440711f3e5edb6ae7112a0f
adityamahapatra <adi.mahapatra@gmail.com>
2020-04-18 06:43:36 +0530

    Replace the main application icon.

--- expense_tracker/src/constants.py
+++ expense_tracker/src/constants.py
@@ -1,4 +1,4 @@
 import os
 
 ICONS_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "icons")
-CREDIT_CARDS = os.path.join(ICONS_PATH, "credit_cards.png")
+CREDIT_CARDS = os.path.join(ICONS_PATH, "expenses.png")
